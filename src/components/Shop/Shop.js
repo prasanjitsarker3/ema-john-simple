a7caf34c7d61815b7d10efb9b8dfda8b4cd8f259
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2021-03-20 01:18:04 +0600

    First ema

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+import { useState } from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) =>
+       console.log('product added',product);
+       
+       
+
+    
+    
+    return (
+        <div className = "shop-container">
+            <div className="product-container">
+                
+                   {
+                    products.map(pd => <Product 
+                        handleAddProduct= {handleAddProduct}
+                        product={pd}
+                        ></Product>)
+                   }
+        
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+                <h5>Order Summary:{cart.length}</h5>
+            </div>
+            
+         </div>
+    );
+};
+
+export default Shop;
